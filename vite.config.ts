d48d729228a34d4b65f650dcf75c8222e992d94d
SrkGrz <srkgrz@gmail.com>
2026-03-02 16:31:26 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Natro ve benzeri hostingler için dosyaların birbirini bulmasını sağlar
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
