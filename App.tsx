1526db6edde5b0013675a06cccaff312381298c6
SrkGrz <srkgrz@gmail.com>
2026-03-04 10:36:56 +0300

    Final fix for /product redirect and Home loading skeletons

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Shop } from './pages/Shop';
 import { Checkout } from './pages/Checkout';
